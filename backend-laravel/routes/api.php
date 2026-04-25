e019a83ffe931d5f8b5b7a10c065373214300188
Ozyura1 <farrelking2@gmail.com>
2026-04-25 17:08:29 +0700

    update

--- backend-laravel/routes/api.php
+++ backend-laravel/routes/api.php
@@ -18,8 +18,8 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 // Auth routes
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
+Route::post('/(auth)/register', [AuthController::class, 'register']);
+Route::post('/(auth)/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
     Route::get('profile', [AuthController::class, 'profile']);
